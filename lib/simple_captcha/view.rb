9374259a7ffc3d649ceb862455a8231ccd5c656c
zhangdabei <keepyouliking@gmail.com>
2012-10-19 20:52:46 +0800

    Update lib/simple_captcha/view.rb
    edti to fit my app

--- lib/simple_captcha/view.rb
+++ lib/simple_captcha/view.rb
@@ -1,5 +1,9 @@
+# Copyright (c) 2008 [Sur http://expressica.com]
+
 module SimpleCaptcha #:nodoc
-  module ViewHelper #:nodoc
+  module ViewHelpers #:nodoc
+    
+    include ConfigTasks
 
     # Simple Captcha is a very simplified captcha.
     #
@@ -13,13 +17,35 @@ module SimpleCaptcha #:nodoc
     #
     # The available options to pass to this method are
     # * label
+    # * image_syle
     # * object
+    # * distortion
     #
     # <b>Label:</b>
     #
     # default label is "type the text from the image", it can be modified by passing :label as
     #
     # <%= show_simple_captcha(:label => "new captcha label") %>.
+    #
+    # <b>Image Style:</b>
+    #
+    # There are eight different styles of images available as...
+    # * embosed_silver
+    # * simply_red
+    # * simply_green
+    # * simply_blue
+    # * distorted_black
+    # * all_black
+    # * charcoal_grey
+    # * almost_invisible
+    #
+    # The default image is simply_blue and can be modified by passing any of the above style as...
+    #
+    # <%= show_simple_captcha(:image_style => "simply_red") %>
+    #
+    # The images can also be selected randomly by using *random* in the image_style as
+    # 
+    # <%= show_simple_captcha(:image_style => "random") %>
     #
     # *Object*
     #
@@ -35,76 +61,80 @@ module SimpleCaptcha #:nodoc
     #
     # *Examples*
     # * controller based
-    # <%= show_simple_captcha(:label => "Human Authentication: type the text from image above") %>
+    # <%= show_simple_captcha(:image_style => "embosed_silver", :label => "Human Authentication: type the text from image above") %>
     # * model based
-    # <%= show_simple_captcha(:object => "person", :label => "Human Authentication: type the text from image above") %>
+    # <%= show_simple_captcha(:object => "person", :image_style => "simply_blue", :label => "Human Authentication: type the text from image above") %>
     #
     # Find more detailed examples with sample images here on my blog http://EXPRESSICA.com
     #
     # All Feedbacks/CommentS/Issues/Queries are welcome.
     def show_simple_captcha(options={})
-      key = simple_captcha_key(options[:object])
-      options[:field_value] = set_simple_captcha_data(key, options)
-      
-      defaults = {
-         :image => simple_captcha_image(key, options),
-         :label => options[:label] || I18n.t('simple_captcha.label'),
-         :field => simple_captcha_field(options)
-         }
-         
-      render :partial => 'simple_captcha/simple_captcha', :locals => { :simple_captcha_options => defaults }
+      options[:field_value] = set_simple_captcha_data(options[:code_type])
+      @simple_captcha_options = 
+        {:image => simple_captcha_image(options),
+         :label => options[:label] || "(type the code from the image)",
+         :field => simple_captcha_field(options)}
+      render :partial => 'simple_captcha/simple_captcha'
+    end
+    def show_update_captcha(options={})
+      options[:field_value] = set_simple_captcha_data(options[:code_type])
+      @update_captcha_options = 
+        {:image => new_captcha_src(options),
+         :label => options[:label] || "(type the code from the image)",
+         :field => simple_captcha_field(options)}
+      render :partial => 'simple_captcha/update_captcha'
     end
 
     private
+    def new_captcha_src(options={})
+      simple_captcha_url(
+          :action => 'simple_captcha',
+          :simple_captcha_key => simple_captcha_key,
+          :image_style => options[:image_style] || '', 
+          :distortion => options[:distortion] || '',
+          :time => Time.now.to_i)
+    end
+    def simple_captcha_image(options={})
+      url = 
+        simple_captcha_url(
+          :action => 'simple_captcha',
+          :simple_captcha_key => simple_captcha_key,
+          :image_style => options[:image_style] || '', 
+          :distortion => options[:distortion] || '',
+          :time => Time.now.to_i)
+      "<img src='#{url}' alt='simple_captcha.jpg' id='captcha_image'/>".html_safe
+    end
+    
+    def simple_captcha_field(options={})
+      options[:object] ?
+      text_field(options[:object], :captcha, :value => '') +
+      hidden_field(options[:object], :captcha_key, {:value => options[:field_value]}) :
+      text_field_tag(:captcha)
+    end
 
-      def simple_captcha_image(simple_captcha_key, options = {})
-        defaults = {}
-        defaults[:time] = options[:time] || Time.now.to_i
-        
-        query = defaults.collect{ |key, value| "#{key}=#{value}" }.join('&')
-        url = "/simple_captcha?code=#{simple_captcha_key}&amp;#{query}"
-        
-        "<img src='#{url}' alt='captcha' />".html_safe
-      end
-      
-      def simple_captcha_field(options={})
-        if options[:object]
-          text_field(options[:object], :captcha, :value => '', :autocomplete => 'off') +
-          hidden_field(options[:object], :captcha_key, {:value => options[:field_value]})
-        else
-          text_field_tag(:captcha, nil, :autocomplete => 'off')
-        end
-      end
-
-      def set_simple_captcha_data(key, options={})
-        code_type = options[:code_type]
-        
-        value = generate_simple_captcha_data(code_type)
-        data = SimpleCaptcha::SimpleCaptchaData.get_data(key)
-        data.value = value
-        data.save
-        key
-      end
-   
-      def generate_simple_captcha_data(code)
-        value = ''
-        
-        case code
-          when 'numeric' then 
-            SimpleCaptcha.length.times{value << (48 + rand(10)).chr}
-          else
-            SimpleCaptcha.length.times{value << (65 + rand(26)).chr}
-        end
-        
-        return value
-      end
-      
-      def simple_captcha_key(key_name = nil)
-        if key_name.nil?
-          session[:captcha] ||= SimpleCaptcha::Utils.generate_key(session[:id].to_s, 'captcha')
-        else
-          SimpleCaptcha::Utils.generate_key(session[:id].to_s, key_name)
-        end
-      end 
+    def set_simple_captcha_data(code_type)
+      key, value = simple_captcha_key, generate_simple_captcha_data(code_type)
+      data = SimpleCaptchaData.get_data(key)
+      data.value = value
+      data.save
+      key
+    end
+ 
+    def generate_simple_captcha_data(code)
+      value = ''
+      #case code
+      #when 'numeric'
+      #  6.times{value << (48 + rand(10)).chr}
+      #else
+      #  6.times{value << (65 + rand(26)).chr}
+      #end
+      4.times{value << (65 + rand(26)).chr}
+      return value
+    end
+ 
   end
+end
+
+ActionView::Base.module_eval do
+  include SimpleCaptcha::ViewHelpers
 end
